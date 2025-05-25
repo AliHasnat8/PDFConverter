8788bfd88378dd369260586b366f30c54183eb6e
AliHasnat8 <alihasnaticloud@gmail.com>
2025-05-25 13:10:16 +0500

    commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# app.py
+from flask import Flask, request, send_file
+from pdf2docx import Converter
+import os
+
+app = Flask(__name__)
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    pdf = request.files['file']
+    pdf_path = 'input.pdf'
+    docx_path = 'output.docx'
+
+    pdf.save(pdf_path)
+
+    cv = Converter(pdf_path)
+    cv.convert(docx_path)
+    cv.close()
+
+    return send_file(docx_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
