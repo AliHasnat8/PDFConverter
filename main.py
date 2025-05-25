f2acad2cdd9ff3bc9cc369cc2d659193b6a07267
AliHasnat8 <alihasnaticloud@gmail.com>
2025-05-25 13:25:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,5 +20,6 @@ def convert():
     return send_file(docx_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
